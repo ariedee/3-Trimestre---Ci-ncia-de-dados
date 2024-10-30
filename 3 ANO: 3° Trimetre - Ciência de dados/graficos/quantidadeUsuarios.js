81544fcf083244e65313d947cc932cab6c0d363c
ariedee <106112918+ariedee@users.noreply.github.com>
2024-10-30 08:12:28 -0300

    Add files via upload

--- 3 ANO: 3° Trimetre - Ciência de dados/graficos/quantidadeUsuarios.js
+++ 3 ANO: 3° Trimetre - Ciência de dados/graficos/quantidadeUsuarios.js
@@ -1,3 +1,5 @@
+import { getCSS, tickConfig } from "./common.js"
+
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
 const res = await fetch(url)
@@ -6,17 +8,52 @@ const nomeDasRedes = object.keys(dados)
 const quantidadeUsuarios = object.values(dados)
 
 const data = [
-    {
-        X: nomeDasRedes, 
+    {'--primary-color'
+        x: nomeDasRedes, 
         y: quantidadedeUsuarios,
-        type: 'bar'
+        type: 'bar',
+        marker: {
+            color: getCSS('--primary-color')
+        }
     }
 ]
+
+const layout = {
+    plot_bgcolor: getCSS ('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+        text:'Redes sociais com mais usuários no mundo',
+        x: 0,
+        font: {
+            color getCSS ('--primary-color'),
+            family: getCSS('--fonte'),
+            size: 30
+        }
+    },
+    xaxis: {
+        tickfont: tickConfig, 
+        title: {
+            text: 'nome das redes sociais',
+            font: {
+                color: getCSS('--secondary-color')
+            }
+        }
+    },
+    yaxis: {
+        tickfont: tickConfig, 
+        title: {
+            text: 'bilhões de usuários ativos',
+            font: {
+                color: getCSS('--secondary-color')
+            }
+        }
+    }
+}
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, layout)
 
 
 }
